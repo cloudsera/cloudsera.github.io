803fe72f72a97e88d5ce7df5f26ee1fcb48825f2
cloudsera <73536543+cloudsera@users.noreply.github.com>
2020-11-18 11:13:33 +0700

    Update script_v1.1.js

--- js/script_v1.1.js
+++ js/script_v1.1.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4007744,4,511,95,18,00000000']);
 _Hasync.push(['Histats.fasi', '1']);
@@ -8,4 +7,3 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
-</script>
